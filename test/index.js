994477adb61c1aa11408359cdbdea68ef2cbee10
Jayesh Salvi <jayeshsalvi@gmail.com>
2017-05-05 13:16:05 -0400

    Add AGPL license header

--- test/index.js
+++ test/index.js
@@ -1,4 +1,25 @@
 
+/*
+
+ Copyright (C) 2017 Jayesh Salvi, Blue Math Software Inc.
+
+ This file is part of Zector Canvas.
+
+ Zector Canvas is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Affero General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ Zector Canvas is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Affero General Public License for more details.
+
+ You should have received a copy of the GNU Affero General Public License
+ along with Zector Canvas.  If not, see <http://www.gnu.org/licenses/>.
+
+ */
+
 import ZCanvas from '../src/zcanvas';
 import {GradientStopList, LinearGradient, RadialGradient} from '../src'
 import {vec2, geom, toDeg, toRad, Transform, Rotation} from 'zmath'
